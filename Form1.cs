3a74cfa46cb477645d89006e0c5ffd040facdaa2
FranciscodeJ <147544324+FranciscodeJ@users.noreply.github.com>
2023-10-20 16:45:46 -0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -14,11 +14,11 @@ namespace Práctica__7
 
         private void button2_Click(object sender, EventArgs e)
         {
-            double v1, cent1;
-            v1 = double.Parse(textBox1.Text);
-            cent1 = (v1 - 32) * (5.0 / 9.0);
-            textBox2.Text = cent1.ToString();
-            label2.Text = " Centígrados";
+            double v1, cent1;                               //Se declararon las varibles para convertir fahrenheit a centígrados.
+            v1 = double.Parse(textBox1.Text);              //Se declaró un valor en el que en la caja 1 se escribirirá. 
+            cent1 = (v1 - 32) * (5.0 / 9.0);                   //Se declara la fórmula de los cantígrados.
+            textBox2.Text = cent1.ToString();                   //Se declara que en la caja 1 se escribirá el resultado de la fórmula.
+            label2.Text = " Centígrados";                     //Se declara que en el cuadro de texto label se escribirá centígrados.
 
         }
 
@@ -34,17 +34,17 @@ namespace Práctica__7
 
         private void button3_Click(object sender, EventArgs e)
         {
-            textBox1.Text = " ";
+            textBox1.Text = " ";                  //Se declara que al oprimir el botón "borrar" se eliminarán los valores resultantes en la caja 1 y 2.
             textBox2.Text = " ";
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double v1, farh1;
-            v1 = double.Parse(textBox1.Text);
-            farh1 = v1 * (9.0 / 5.0) + 32;
-            textBox2.Text = farh1.ToString();
-            label2.Text = " Fahrenheith";
+            double v1, farh1;                                    //Se declararon las varibles para convertir centígrados a fahrenheit.
+            v1 = double.Parse(textBox1.Text);                    //Se declara que en la caja 1 se escribirá los centígrados.
+            farh1 = v1 * (9.0 / 5.0) + 32;                       //Se declara la fórmula.
+            textBox2.Text = farh1.ToString();                    //Se declara que en la caja 2 aparecerá el resultado de la fórmula.
+            label2.Text = " Fahrenheith";                       //Se declara que en el cuadro de texto label aparecerán los grados fahrenheith del resultado.
 
 
 
